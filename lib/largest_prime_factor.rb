c59bad588ae088b60d49de38e308df024704c6de
Sara Stanton <sara.mpa13@gmail.com>
2019-03-31 23:14:52 -0500

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,12 @@
 # Enter your procedural solution here!
+def find_factors(num)
+  Array(1..Math.sqrt(num)).select{|x| num % x == 0}
+end
+
+def is_prime?(num)
+  (2..num/2).none?{|x| num % x == 0 }
+end
+
+def largest_prime_factor(num)
+  find_factors(num).select{|x| is_prime?(x)}.max
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+
+class LargestPrimeFactor
+
+  attr_accessor :num
+
+  def initialize(num)
+    @num = num
+  end
+
+  def find_factors
+    Array(1..Math.sqrt(self.num)).select{|x| num % x == 0}
+  end
+
+  def is_prime?(n)
+    (2..n/2).none?{|x| n % x == 0 }
+  end
+
+  def number
+    find_factors.select{|x| is_prime?(x)}.max
+  end
+
+end
